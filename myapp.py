c1e98ee8835bdf0e61c9a0bfbf2ecf2bd45cdf68
richard atsyor <richardatsyor@gmail.com>
2017-07-19 14:57:52 +0000

    to send a msg mqtt

--- myapp.py
+++ myapp.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
+import os
+import paho.mqtt.client as mqtt
 
+client = mqtt.Client()
+client.connect("moorhouseassociaties.com" 1883, 60)
 
 app = Flask(__name__)
 
@@ -14,6 +18,11 @@ def whereami():
 @app.route('/hello/<name>')
 def foo(name):
 	return render_template('index.html', to=name)
+
+@app. route('/btn')
+def index():
+	print("button clicked")
+	client .publish("test/all" , "this is my message to you")
 
 @app.route('/linux')
 def mylinux():
